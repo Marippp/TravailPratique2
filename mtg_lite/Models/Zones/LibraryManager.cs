5fdae6355edaa55e773f80cea13defbb2b9715ce
charron1403 <alexwillgo2@gmail.com>
2022-11-09 10:22:46 -0500

    library deck added

--- mtg_lite/Models/Zones/LibraryManager.cs
+++ mtg_lite/Models/Zones/LibraryManager.cs
@@ -5,12 +5,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace mtg_lite.Models.Zones
 {
     public static class LibraryManager
     {
-        private static Dictionary<string, List<Card>> libraries = new Dictionary<string, List<Card>>();
+        private static Dictionary<string, List<Card>> libraries = new Dictionary<string, List<Card>>()
+        {
+            {"deck", new List<Card>(
+                Creature.FabriquerCarteCreature("aegis_turtle"),
+                Creature.FabriquerCarteCreature("barony_vampire"),
+                Creature.FabriquerCarteCreature("scathe_zombies"),
+                Land.FabriquerCarteLand("swamp"),
+                Land.FabriquerCarteLand("mountain"),
+                Land.FabriquerCarteLand("forest"),
+                Land.FabriquerCarteLand("island"),
+                Sorcery.FabriquerCarteSorcery("incriminate"),
+                Sorcery.FabriquerCarteSorcery("glimpse_the_unthinkable"),
+                Sorcery.FabriquerCarteSorcery("blightning"))}
+        };
 
         static LibraryManager()
         {
