9c436dd9939e2943261b1ea094c021eedeb70808
NathanSylvain <sylvain.nathan33@gmail.com>
2022-11-09 09:23:55 -0500

    commencer Tapped

--- mtg_lite/Models/Cards/Land.cs
+++ mtg_lite/Models/Cards/Land.cs
@@ -9,26 +9,31 @@ namespace mtg_lite.Models.Cards
 {
     public class Land : Card
     {
+        public bool tapped;
+
+
         public Land(string name, Mana manaCost, Bitmap picture) : base(name, manaCost, picture)
         {
         }
+
+        public bool Tapped { get => tapped;}
 
         static public Land FabriquerCarteLand(string name)
         {   
             switch (name)
             {
                 case "swamp":
-                    return new Land("swamp", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/swamp.png")));
+                    return new Land("swamp", new Mana(0, 0, 0, 0, 0, 0),Resource.swamp);
                     
                 case "island":
-                    return new Land("island", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/island.png")));
+                    return new Land("island", new Mana(0, 0, 0, 0, 0, 0), Resource.island);
                     
                 case "mountain":
-                    return new Land("mountain", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/mountain.png")));
+                    return new Land("mountain", new Mana(0, 0, 0, 0, 0, 0), Resource.mountain);
                 case "forest":
-                    return new Land("mountain", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/forest.png")));
+                    return new Land("mountain", new Mana(0, 0, 0, 0, 0, 0), Resource.forest);
                 default:
-                    return new Land("swamp", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/swamp.png")));
+                    return new Land("swamp", new Mana(0, 0, 0, 0, 0, 0), Resource.swamp);
 
             }
         }
