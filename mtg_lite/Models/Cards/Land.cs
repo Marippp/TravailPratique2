3acadf50d7805e7d853d50c5833140c0248dad71
NathanSylvain <sylvain.nathan33@gmail.com>
2022-11-09 09:18:15 -0500

    Créé les land
    Factory qui crée les land

---
+++ mtg_lite/Models/Cards/Land.cs
@@ -0,0 +1,36 @@
+﻿using MTGO_lite.Models.Manas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtg_lite.Models.Cards
+{
+    public class Land : Card
+    {
+        public Land(string name, Mana manaCost, Bitmap picture) : base(name, manaCost, picture)
+        {
+        }
+
+        static public Land FabriquerCarteLand(string name)
+        {   
+            switch (name)
+            {
+                case "swamp":
+                    return new Land("swamp", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/swamp.png")));
+                    
+                case "island":
+                    return new Land("island", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/island.png")));
+                    
+                case "mountain":
+                    return new Land("mountain", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/mountain.png")));
+                case "forest":
+                    return new Land("mountain", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/forest.png")));
+                default:
+                    return new Land("swamp", new Mana(0, 0, 0, 0, 0, 0), new Bitmap(Image.FromFile("Resource/swamp.png")));
+
+            }
+        }
+    }
+}
